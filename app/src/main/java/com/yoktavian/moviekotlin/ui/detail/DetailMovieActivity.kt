d8b3e7729667eca4da603e6d42bdf1c297a067fb
yoktavian <yoktavian17@gmail.com>
2018-06-21 17:25:17 +0700

    fixing view model

--- app/src/main/java/com/yoktavian/moviekotlin/ui/detail/DetailMovieActivity.kt
+++ app/src/main/java/com/yoktavian/moviekotlin/ui/detail/DetailMovieActivity.kt
@@ -7,7 +7,6 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import com.bumptech.glide.Glide
--- app/src/main/java/com/yoktavian/moviekotlin/viewmodel/HomeV1ViewModel.kt
+++ app/src/main/java/com/yoktavian/moviekotlin/viewmodel/HomeV1ViewModel.kt
@@ -5,7 +5,6 @@ import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import com.yoktavian.moviekotlin.data.repository.MovieRepository
 import com.yoktavian.moviekotlin.remote.response.MovieResponse
-import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
@@ -16,16 +15,14 @@ class HomeV1ViewModel : ViewModel() {
         LOADING, SERVER_ERROR, INTERNET_ERROR, SUCCESS
     }
     private val movieRepo : MovieRepository = MovieRepository()
-    private val disposable : Disposable? = null
+    private var disposable : Disposable? = null
     private val movies : MutableLiveData<MovieResponse> = MutableLiveData()
-    private var moviesResponse : Observable<MovieResponse>? = null
     // experiment view
     private var stateOfView : MutableLiveData<StateOfView> = MutableLiveData()
 
     fun getMovieNowPlaying(page : Int) : LiveData<MovieResponse> {
         setState(StateOfView.LOADING)
-        moviesResponse = movieRepo.getMoviesNowPlaying(page)
-        moviesResponse!!.subscribeOn(Schedulers.io())
+        disposable = movieRepo.getMoviesNowPlaying(page).subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({
                     result -> movies.value = result
@@ -50,8 +47,8 @@ class HomeV1ViewModel : ViewModel() {
     }
 
     override fun onCleared() {
-        if (disposable != null && !disposable.isDisposed)
-            disposable.dispose()
+        if (disposable != null && disposable!!.isDisposed)
+            disposable!!.dispose()
         super.onCleared()
     }
 }
